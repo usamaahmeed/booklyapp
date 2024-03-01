8902025fa159bff89e679dc45eea9564785a9aa6
osama <osama@gmail.com>
2024-03-01 17:37:04 +0200

    Initial commit

---
+++ lib/Features/Splash/presentation/views/widgets/splash_screen_body.dart
@@ -0,0 +1,17 @@
+import 'package:booklyapp/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreenBody extends StatelessWidget {
+  const SplashScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(AssetsData.logo),
+      ],
+    );
+  }
+}
